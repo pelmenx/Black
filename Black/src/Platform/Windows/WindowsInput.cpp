61dbb2c8cbf35a77758a2ab3def8cd497c1472cf
Vladislav Talantsev <vladislav.talantsev@ubisoft.com>
2024-09-22 19:01:03 +0200

    change auto values in WindowsInput.cpp

--- Black/src/Platform/Windows/WindowsInput.cpp
+++ Black/src/Platform/Windows/WindowsInput.cpp
@@ -13,23 +13,23 @@ namespace Black
     
     bool WindowsInput::IsKeyPressedImpl(int keycode)
     {
-        auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+        GLFWwindow* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 
-        auto state = glfwGetKey(window, keycode);
+        int state = glfwGetKey(window, keycode);
         return state == GLFW_PRESS || state == GLFW_REPEAT;
     }
 
     bool WindowsInput::IsMouseButtonPressedImpl(int button)
     {
-        auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+        GLFWwindow* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 
-        auto state = glfwGetMouseButton(window, button);
+        int state = glfwGetMouseButton(window, button);
         return state == GLFW_PRESS;
     }
 
     std::pair<float, float> WindowsInput::GetMousePositionImpl()
     {
-        auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+        GLFWwindow* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         double posX, posY;
         glfwGetCursorPos(window, &posX, &posY);
         
